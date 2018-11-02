19f7eed8e944d8e72a5e3e68ade15cb7eeddaf04
Elly <elly@mg.crane.ai>
2018-11-02 11:39:17 -0400

    added tests/share refactoring

---
+++ tests/share.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - share UNIT TESTS
+ * Application Client
+ * Nov 2
+*/
+
+/* the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action */
+it("the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the search view has a notification icon located at the bottom right of the view which on click will trigger the notification() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
