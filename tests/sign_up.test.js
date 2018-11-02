98fca605c6b91dd77292d8a6287a7e05da3bde1c
Elly <elly@mg.crane.ai>
2018-11-02 11:55:59 -0400

    added tests/sign_up refactoring

---
+++ tests/sign_up.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - sign up UNIT TESTS
+ * Application Client
+ * Nov 2
+*/
+
+/* the history view has a like icon located at the  center of the view which on click will trigger the like() action */
+it("the history view has a like icon located at the  center of the view which on click will trigger the like() action", () => {
+  /* ToDo developer needs to complete unit test 
+               the history view has a like icon located at the  center of the view which on click will trigger the like() action
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
