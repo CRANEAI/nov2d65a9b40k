43c2418fa28d3587592a8c54a351c2e25c8b966a
Elly <elly@mg.crane.ai>
2018-11-02 12:01:54 -0400

    updated tests/search refactoring

--- tests/search.test.js
+++ tests/search.test.js
@@ -7,10 +7,10 @@
  * Nov 2
 */
 
-/* view has actions which trigger on events, view should should error messages and display activity indicator when processing */
-it("view has actions which trigger on events, view should should error messages and display activity indicator when processing", () => {
+/* the history view has a like icon located at the  center of the view which on click will trigger the like() action */
+it("the history view has a like icon located at the  center of the view which on click will trigger the like() action", () => {
   /* ToDo developer needs to complete unit test 
-               view has actions which trigger on events, view should should error messages and display activity indicator when processing
+               the history view has a like icon located at the  center of the view which on click will trigger the like() action
                uncomment below and update the code to test
                 //instance.action(function(error) {
                 //  if (error) throw error;
