8ae94865e58af0a00f753ea7facb8a485ab34f56
Elly <elly@mg.crane.ai>
2018-11-02 11:39:20 -0400

    added tests/payment refactoring

---
+++ tests/payment.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - payment UNIT TESTS
+ * Application Client
+ * Nov 2
+*/
+
+/* the payment view has a Input Box located at the main area across the full width of the view, correct text needs defined */
+it("the payment view has a Input Box located at the main area across the full width of the view, correct text needs defined", () => {
+  /* ToDo developer needs to complete unit test 
+               the payment view has a Input Box located at the main area across the full width of the view, correct text needs defined
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
