b668f9e2955294745cfdf04cb134de94b16c79d2
Elly <elly@mg.crane.ai>
2018-11-02 11:43:47 -0400

    added client/src/components/search refactoring

---
+++ client/src/components/search.jsx
@@ -0,0 +1,349 @@
+
+/*
+search view
+created by: FusionCode
+
+//----------------------------------------------------------------------------------------------------------
+// Component Requirements
+//----------------------------------------------------------------------------------------------------------
+
+the sign up view has a status bar located at the very-top across the full width of the view
+the sign up view has a Headline Label located at the top left of the view, correct text needs defined
+sign up view has Label located at the top left of the view, text needs identified
+sign up view has Label located at the top left of the view, text needs identified
+sign up view has Label located at the top left of the view, text needs identified
+sign up view has Label located at the mid-top left of the view, text needs identified
+sign up view has Label located at the mid-top left of the view, text needs identified
+the sign up view has a Input Box with text :l located at the mid-top in the center of the view
+sign up view has Label located at the main area left of the view, text needs identified
+the sign up view has a Pager located at the main area left of the view, correct text needs defined
+sign up view has a Label located at the mid-bottom center of the view with text ﬂ
+sign up view has Label located at the bottom center of the view, text needs identified
+sign up view has Label located at the bottom center of the view, text needs identified
+view has actions which trigger on events, view should should error messages and display activity indicator when processing
+
+*/
+        
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Row, Col , Input, InputGroup, InputGroupText } from 'reactstrap';
+        
+        
+        import { close,_action, close,_action, close,_action, close,_action, close,_action, close,_action, close_action, close_action, close_action, close_action, close_action, close_action }  from '../actions/searchActions.js'; 
+
+        
+        
+       
+        class searchView extends React.Component {
+
+            state = {
+                 show_loading_indicator: false,
+                search : '', 
+
+            }
+    
+            
+            // help event
+            help = () => {
+                
+                 help = () => {
+                    <!-- ToDo: define what help will do in the help component --> 
+                 })
+                    
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close, event
+            close, = (value) => {
+                const { close,_action } = this.props
+                        e.preventDefault()
+
+                        let close,_data      =  {};
+				close,_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close,_action(close,_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+            // close event
+            close = (value) => {
+                const { close_action } = this.props
+                        e.preventDefault()
+
+                        let close_data      =  {};
+				close_data.search   = this.state.search 
+
+
+                        //Dispatch action mapped to redux
+                        close_action(close_data)
+                        
+                        
+                        // Change state of activity indicator
+                        this.setState({
+                            show_loading_indicator: true
+                        })
+                        
+              }
+        handleChange(e) {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }    
+            
+
+            componentDidMount = () => {
+                
+               
+                
+            }
+    
+        render() {
+        
+         const { show_loading_indicator } = this.state
+        const { data } = this.props
+
+            return (
+                <Row>
+
+                {!data.error_message &&
+                    show_loading_indicator === true ? <i className="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i> : 
+                    <Col md={12}>
+
+                        			<Col md={12}>
+				<InputGroup>					<Input 
+                                        className="form-group input-light turquoise-block" 
+                                        value={this.state.search}
+                                        onChange={this.handleChange}
+                                        name="search"
+                                        placeholder="search" />				</InputGroup>			</Col>
+
+			<Col md={12}>
+				<p className="label-gray-block-small"> 123 </p>
+			</Col>
+
+			<Col md={12}>
+				<p className="label-white-standard-small"> space </p>
+			</Col>
+
+
+                           
+   
+                    </Col> 
+                     } 
+                </Row>
+            )   
+        }
+    }
+                   
+    function mapStateToProps(state) {
+            return {
+                    authentication: state.authentication,
+                    data: state.search
+                
+            }
+        }
+    
+        function mapDispatchToProps(dispatch) {
+            return bindActionCreators({ close,_action, close,_action, close,_action, close,_action, close,_action, close,_action, close_action, close_action, close_action, close_action, close_action, close_action  }, dispatch);
+        }
+    
+   
+        export default connect(mapStateToProps, mapDispatchToProps)(searchView);
+    
+        
